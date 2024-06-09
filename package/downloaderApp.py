c19d4f1bb2332c92d1e781af7ff036ef4e1bc8ad
zhackk <110896434+zhackk@users.noreply.github.com>
2024-06-10 02:47:06 +0300

    some fixes here and there

--- package/downloaderApp.py
+++ package/downloaderApp.py
@@ -1,4 +1,16 @@
+import kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.spinner import Spinner
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.uix.filechooser import FileChooserIconView
+
 
 class DownloaderApp(App):
     def build(self):
